f2be1661880654dbf69c0deeaedbb4a6502423eb
JararvisQ <286980036@qq.com>
2021-10-22 23:11:32 +0800

    动态规划，分治补充

---
+++ 算法/template/divideConquer.js
@@ -0,0 +1,20 @@
+function divideConquer() {
+  // 终止条件
+  if (res < target) {
+    return
+  }
+
+  // 分割问题
+  data = prepareData(problem)
+  subProblems = splitProblem(problem, data)
+
+  // 处理子问题
+  subresult1 = divideConquer(subProblems[0], p1)
+  subresult2 = divideConquer(subProblems[1], p1)
+  subresult3 = divideConquer(subProblems[2], p1)
+
+  // 合成最后结果
+  result = mergeResult(subresult1, subresult2, ...)
+
+  // 处理当前层状态: 恢复等
+}
